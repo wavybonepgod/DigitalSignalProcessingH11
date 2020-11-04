ff4985109f55869329aba91668eaea811312c57e
ilbagrus <ilbagrus@gmail.com>
2020-11-04 21:20:56 +0300

    HW4

--- HW4/hw4_bagautdinov_ilyas_09-741.py
+++ HW4/hw4_bagautdinov_ilyas_09-741.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.polynomial.polynomial as pol
 from scipy.io import wavfile
 from scipy.signal import correlate
 
-rate, voice = wavfile.read("watermark.wav")
+voice = wavfile.read("watermark.wav")[1]
 
 s = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]).reshape((-1, 1))
 d = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
 
-companion_matrix = np.polynomial.polynomial.polycompanion([1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]).T.astype(int)
+companion_matrix = pol.polycompanion([1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]).T.astype(int)
 
 water = np.zeros(2 ** 11 - 2, dtype=int)
 
 for i in range(1, 2 ** 11 - 1):
     s = companion_matrix @ s
-    w = np.mod(d @ s, 2)
+    w = d @ s % 2
     if w == 0:
         w = -1
     water[i - 1] = w
